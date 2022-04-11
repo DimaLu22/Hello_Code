6c7c4aa0b0c8d6cff69dcfb123f04d8f42bf3d7c
Dmitry Lushchik <dmitripl@mail.ru>
2022-04-11 17:11:37 +0300

    Add RANDOM

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,5 @@
-﻿int numberA = 3;
-int numberB = 5;
-int result = numberA + numberB;
-Console.WriteLine(result);
+﻿double numberA = new Random().Next(1,10); // Случайное число от 1 до 9
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA + numberB);
