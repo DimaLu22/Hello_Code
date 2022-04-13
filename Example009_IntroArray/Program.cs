fd2fe594c44857033e5ab0cabe37daeaad0767bb
Dmitry Lushchik <dmitripl@mail.ru>
2022-04-13 16:57:21 +0300

    Add Example 010 MethodArray

--- Example009_IntroArray/Program.cs
+++ Example009_IntroArray/Program.cs
@@ -22,7 +22,7 @@ int max = Max(
                                                                             //if(b1 > max) max = b1;
                                                                             //if(c1 > max) max = c1;
 
-                                                                            //if(a2 > max) max = a2;
+                                                                            //if(a2 > max) max = a2;git
                                                                             //if(b2 > max) max = b2;
                                                                             //if(c2 > max) max = c2;
 
---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {1, 12, 31, 4, 15, 16, 17, 18};
+
+int n = array.Length; //Узнаем сколько эллементов в массиве
+int find = 4; //искомое число в массиве
+
+int index = 0;
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; //добавляется если нужно найти только первый
+    }
+    index++;
+}
