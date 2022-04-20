8beea32f3244ab4f1e1d5becde434b1b89d0072a
Dmitry Lushchik <dmitripl@mail.ru>
2022-04-20 13:13:11 +0300

    Add SottArray

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -81,32 +81,83 @@
 // Дан текст. В тексте нужно все пробелы заменить четрочками, маленькие буквы "к" заменить большими "К",
 // а большие "С" заменить маленькими "с".
 
-string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
-            + "ежели вас послали вместо нашего милого Винцингероде,"
-            + "вы бы взяли приступом согласие прусского короля. "
-            + "Вы так красноречивы. Вы дадите мне чаю?";
+//string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+//            + "ежели вас послали вместо нашего милого Винцингероде,"
+//            + "вы бы взяли приступом согласие прусского короля. "
+//            + "Вы так красноречивы. Вы дадите мне чаю?";
 
-// string s = "qwerty"
-// s[3] // r
+//// string s = "qwerty"
+//// s[3] // r
 
-string Replace(string text, char oldValue, char newValue)
+//string Replace(string text, char oldValue, char newValue)
+//{
+//    string result = String.Empty;
+//    int length = text.Length;
+//    for(int i = 0; i < length; i++)
+//    {
+//        if(text[i] == oldValue)
+//        result = result + $"{newValue}";
+//        else result = result + $"{text[i]}";
+//
+//    }
+//    return result;
+//}
+//string newText = Replace(text, ' ', '|');
+//Console.WriteLine(newText);
+//Console.WriteLine();
+//newText = Replace(newText, 'к', 'К');
+//Console.WriteLine(newText);
+//Console.WriteLine();
+//newText = Replace(newText, 'С', 'с');
+//Console.WriteLine(newText);
+
+//Метод сортировки массива Минмакс
+
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray(int[] array)
 {
-    string result = String.Empty;
-    int length = text.Length;
-    for(int i = 0; i < length; i++)
+    int count = array.Length;
+    for(int i = 0; i < count; i++)
     {
-        if(text[i] == oldValue)
-        result = result + $"{newValue}";
-        else result = result + $"{text[i]}";
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+PrintArray(arr);
+
+void SelectionSortMinMax(int[] array)
+{
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+        int minposition = i;
+            for(int j = i +1; j < array.Length; j++)
+            {
+                if(array[j] < array[minposition])  minposition = j;
+            }
+        int temporary = array[i];
+        array[i] = array[minposition];
+        array[minposition] = temporary;
 
     }
-    return result;
 }
-string newText = Replace(text, ' ', '|');
-Console.WriteLine(newText);
-Console.WriteLine();
-newText = Replace(newText, 'к', 'К');
-Console.WriteLine(newText);
-Console.WriteLine();
-newText = Replace(newText, 'С', 'с');
-Console.WriteLine(newText);
+SelectionSortMinMax(arr);
+PrintArray(arr);
+
+void SelectionSortMaxMin(int[] array)
+{
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+        int maxposition = i;
+            for(int j = i +1; j < array.Length; j++)
+            {
+                if(array[j] > array[maxposition])  maxposition = j;
+            }
+        int temporary = array[i];
+        array[i] = array[maxposition];
+        array[maxposition] = temporary;
+
+    }
+}
+SelectionSortMaxMin(arr);
+PrintArray(arr);
